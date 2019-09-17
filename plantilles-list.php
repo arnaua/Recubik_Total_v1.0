7a4b6656e7fb0a1529fc2fda9d2c8116ef931e56
Arnau Amadó <arnaua@recubik.com>
2019-09-17 15:13:00 +0200

    2

--- plantilles-list.php
+++ plantilles-list.php
@@ -38,7 +38,7 @@
                         <div class="product-status-wrap">
                             <h4>Listado de Plantillas</h4>
                             <div class="add-product">
-                                <a href="#">Añadir Plantillas</a>
+                                <a href="#">Añadir Plantilla</a>
                             </div>
                             <div class="asset-inner">
                                 <table>
